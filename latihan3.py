d30e5201ef8fa9df1faf8b481cb9979f3ed21eb7
cousin-rifki <rifkiibul@gmail.com>
2023-11-14 13:29:50 +0700

    Belajarpython

---
+++ latihan3.py
@@ -0,0 +1,7 @@
+panjang = int(input("masukkan panjang nya: "))
+lebar = int(input("masukkan lebar nya: "))
+tinggi =int(input("masukkan tinggi nya: "))
+
+isi = panjang*lebar*tinggi
+
+print ("hasilnya adalah :",isi)
